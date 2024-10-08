69992363abf2c9da4fba1898a9ecfd5aa6e98864
tebogo60 <tjjmalebana@gmail.com>
2024-10-09 01:41:09 +0200

    initial commit - initialize spring boot and thymeleaf

---
+++ src/main/java/com/typerx/typerx/TyperxApplication.java
@@ -0,0 +1,13 @@
+package com.typerx.typerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyperxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyperxApplication.class, args);
+	}
+
+}
